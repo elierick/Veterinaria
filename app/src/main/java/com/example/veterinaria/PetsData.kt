27f303580d28613ae6b16159326ad59c123b6cfa
Erick PC <elierick2013@gmail.com>
2020-11-10 14:18:53 -0600

    creación de todas las activitys

---
+++ app/src/main/java/com/example/veterinaria/PetsData.kt
@@ -0,0 +1,11 @@
+package com.example.veterinaria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PetsData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pets_data)
+    }
+}
