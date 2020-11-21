21bada17c7437b65525111de7873999c9d707e21
ERICK <elierick2013@gmail.com>
2020-11-21 12:08:11 -0600

    add nueva cita

--- app/src/main/java/com/example/veterinaria/ui/general/NewAppointment.kt
+++ app/src/main/java/com/example/veterinaria/ui/general/NewAppointment.kt
@@ -10,11 +10,13 @@ import com.example.veterinaria.R
 
 class NewAppointment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_new_appointment, container, false)
     }
